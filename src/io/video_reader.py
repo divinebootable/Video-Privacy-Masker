13cd2e5b9d383fede4a99b8e24fe0042ee14a05a
Monyuy Divine Kongadzem <dbootable@gmail.com>
2026-08-02 10:37:58 +0100

    video reader

---
+++ src/io/video_reader.py
@@ -0,0 +1,10 @@
+
+
+class VideoReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+    
+    
+    def get_frames(self):
+        # Read a single frame from the video file
+    
